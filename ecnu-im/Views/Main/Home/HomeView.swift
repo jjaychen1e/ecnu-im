728238212e78961d5bac31c4c763ab05d8e1e2e2
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2022-06-03 23:04:10 +0800

    fix: Typo(回复了你了)

--- ecnu-im/Views/Main/Home/HomeView.swift
+++ ecnu-im/Views/Main/Home/HomeView.swift
@@ -255,7 +255,7 @@ struct HomeView: View {
                                             return "Unkown"
                                         }()
                                         let latestNotificationTypeDescription = notifications.first?.attributes.contentType.actionDescription ?? "Unkown"
-                                        viewModel.latestNotificationTitle = "@\(latestNotificationUserName)\(latestNotificationTypeDescription)了：\(latestNotificationDiscussionTitle)"
+                                        viewModel.latestNotificationTitle = "@\(latestNotificationUserName)\(latestNotificationTypeDescription)：\(latestNotificationDiscussionTitle)"
                                     case .badgeReceived:
                                         if let subject = notifications[0].relationships?.subject {
                                             if case let .userBadge(userBadgeId) = subject {
