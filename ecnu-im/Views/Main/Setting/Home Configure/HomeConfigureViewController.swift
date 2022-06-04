618cfcc8b877de68912e42454f31c62f3e0fb1e4
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2022-06-04 15:20:36 +0800

    fix: Home configuration setting appear synchronously(No drawing needed)

--- ecnu-im/Views/Main/Setting/Home Configure/HomeConfigureViewController.swift
+++ ecnu-im/Views/Main/Setting/Home Configure/HomeConfigureViewController.swift
@@ -20,46 +20,33 @@ private class InnerHomeConfigureViewController: SettingViewController, NoOverlay
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "首页模块配置"
-    }
 
-    private var dataSourceLoaded = false
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        if !dataSourceLoaded {
-            dataSourceLoaded = true
-            DispatchQueue.main.async { [weak self] in
-                if let self = self {
-                    self.modelObjects = [
-                        HeaderItem.hidden(rowItems: [
-                            RowItem(type: .toggle(action: { value in
-                                                      AppGlobalState.shared.showRecentActiveUsers.send(value)
-                                                  },
-                                                  publisher: AppGlobalState.shared.showRecentActiveUsers.eraseToAnyPublisher()),
-                                    icon: .system(name: "person.crop.circle"),
-                                    label: "最近活跃用户"),
-                            RowItem(type: .toggle(action: { value in
-                                                      AppGlobalState.shared.showRecentOnlineUsers.send(value)
-                                                  },
-                                                  publisher: AppGlobalState.shared.showRecentOnlineUsers.eraseToAnyPublisher()),
-                                    icon: .uiImage(uiImage: {
-                                        let config = UIImage.SymbolConfiguration(paletteColors: [UIColor("#7FBA00"), Asset.DynamicColors.dynamicBlack.color])
-                                        let image = UIImage(systemName: "person.crop.circle.badge", withConfiguration: config)!
-                                        return image
-                                    }(), color: nil),
-                                    label: "最近在线用户（若有权限）"),
-                            RowItem(type: .toggle(action: { value in
-                                                      AppGlobalState.shared.showRecentRegisteredUsers.send(value)
-                                                  },
-                                                  publisher: AppGlobalState.shared.showRecentRegisteredUsers.eraseToAnyPublisher()),
-                                    icon: .system(name: "person.crop.circle.badge.plus"),
-                                    label: "最近注册用户（若有权限）"),
-                        ]),
-                    ]
-                    self.applyInitialSnapshots()
-                }
-            }
-        }
+        modelObjects = [
+            HeaderItem.hidden(rowItems: [
+                RowItem(type: .toggle(action: { value in
+                                          AppGlobalState.shared.showRecentActiveUsers.send(value)
+                                      },
+                                      publisher: AppGlobalState.shared.showRecentActiveUsers.eraseToAnyPublisher()),
+                        icon: .system(name: "person.crop.circle"),
+                        label: "最近活跃用户"),
+                RowItem(type: .toggle(action: { value in
+                                          AppGlobalState.shared.showRecentOnlineUsers.send(value)
+                                      },
+                                      publisher: AppGlobalState.shared.showRecentOnlineUsers.eraseToAnyPublisher()),
+                        icon: .uiImage(uiImage: {
+                            let config = UIImage.SymbolConfiguration(paletteColors: [UIColor("#7FBA00"), Asset.DynamicColors.dynamicBlack.color])
+                            let image = UIImage(systemName: "person.crop.circle.badge", withConfiguration: config)!
+                            return image
+                        }(), color: nil),
+                        label: "最近在线用户（若有权限）"),
+                RowItem(type: .toggle(action: { value in
+                                          AppGlobalState.shared.showRecentRegisteredUsers.send(value)
+                                      },
+                                      publisher: AppGlobalState.shared.showRecentRegisteredUsers.eraseToAnyPublisher()),
+                        icon: .system(name: "person.crop.circle.badge.plus"),
+                        label: "最近注册用户（若有权限）"),
+            ]),
+        ]
+        applyInitialSnapshots()
     }
 }
