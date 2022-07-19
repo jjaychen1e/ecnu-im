4202dcdd4a560beb724d29d7037c30e478a86496
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2022-07-19 20:25:25 +0800

    fix: iPadOS 16 animation

--- ecnu-im/Views/Main/Discussion/DiscussionViewController.swift
+++ ecnu-im/Views/Main/Discussion/DiscussionViewController.swift
@@ -325,14 +325,8 @@ class DiscussionViewController: UIViewController, NoOverlayViewController, HasNa
             let cell = tableView.dequeueReusableCell(withIdentifier: PostCommentCell.identifier, for: indexPath) as! PostCommentCell
             cell.configure(discussion: discussion, post: post, viewController: self) {
                 DispatchQueue.main.async {
-                    if #available(iOS 16, *) {
-                        // From iOS 16, UIKit will auto resize cell after call cell or contentView's `invalidateIntrinsicContentSize`.
-                        // Using AutoLayout, it will update automatically.
-                        cell.invalidateIntrinsicContentSize()
-                    } else {
-                        UIView.performWithoutAnimation {
-                            tableView.reconfigureRows(at: [indexPath])
-                        }
+                    UIView.performWithoutAnimation {
+                        tableView.reconfigureRows(at: [indexPath])
                     }
                 }
             } replyPostAction: { [weak self] in
